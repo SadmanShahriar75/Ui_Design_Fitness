d7b19f0694dd909697ae859360ec9771de28bdbf
SadmanShahriar75 <130807436+SadmanShahriar75@users.noreply.github.com>
2023-04-17 03:36:35 +0600

    Add files via upload

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,261 @@
+import 'package:fitness_app/screens/mode.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  final List<Category>catego =[
+   Category(
+      imagUrl: "assets/images/emily.png",
+      name: "Yoga exercises",
+    ),
+    Category(
+      imagUrl: "assets/images/sule.png",
+      name: "Example exercises",
+    ),
+    Category(
+      imagUrl: "assets/images/alexsandra.png",
+      name: "Example exercises",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/picture2.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 60.0, left: 20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "hey,",
+                          style: GoogleFonts.bebasNeue(
+                            fontSize: 32,
+                            color: Colors.white,
+                            letterSpacing: 1.8,
+                          ),
+                        ),
+                        Text(
+                          "Zezo",
+                          style: GoogleFonts.bebasNeue(
+                            fontSize: 32,
+                            color: Color(0xFF40D876),
+                            letterSpacing: 1.8,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 42,
+                      height: 42,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                          width: 3,
+                          color: Color(0xFF40D876),
+                        ),
+                        image: const DecorationImage(
+                            image: AssetImage("assets/images/emely.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70.0),
+                child: Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.1),
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  child: Center(
+                    child: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF40D876),
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20, top: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Find ",
+                          style: GoogleFonts.lato(
+                            fontSize: 26,
+                            color: Color(0xFF40D876),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "your Workout",
+                          style: GoogleFonts.lato(
+                            fontSize: 26,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                   const Icon(
+                      Icons.filter_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  width: 353,
+                  height: 46,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF232441),
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "SEARCH WORKOUT",
+                          hintStyle: TextStyle(color: Colors.white54),
+                          border: InputBorder.none,
+                          icon: Icon(
+                            Icons.search,
+                            size: 30,
+                            color: Colors.white,
+                          )),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Popular",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                    Text("Hard workout",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                    Text("Full Body",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                    Text("Crossfit",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    Text("Popular Workout",
+                              style: GoogleFonts.lato(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              )),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: ListView.builder(
+                      itemCount: catego.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 172,
+                                width: 141,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(catego[index].imagUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                catego[index].name,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+           
+           
+           ),
+           )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
